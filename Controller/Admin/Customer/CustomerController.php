bf786baa8c08bd3b5b8a247f57f2edaff4b1e045
s.akiyama <s.akiyama@nskint.co.jp>
2023-02-15 23:34:25 +0900

    20230215分
    顧客イベント管理画面登録更新作成
    Master系エンティティの追加勉強

--- Controller/Admin/Customer/CustomerController.php
+++ Controller/Admin/Customer/CustomerController.php
@@ -22,11 +22,14 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Form;
+use Plugin\management\Form\Type\Admin\Customer\SearchCustomerEventType;
 use Plugin\management\Form\Type\Admin\Customer\CustomerEventType;
+use Plugin\management\Entity\Customer\CustomerEvent;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CustomerController extends AbstractController
 {
-
     /**
      * @var PageMaxRepository
      */
@@ -44,8 +47,7 @@ class CustomerController extends AbstractController
     public function __construct(
         PageMaxRepository $pageMaxRepository,
         CustomerEventRepository $customerEventRepository
-    )
-    {
+    ) {
         $this->pageMaxRepository = $pageMaxRepository;
         $this->customerEventRepository = $customerEventRepository;
     }
@@ -59,7 +61,7 @@ class CustomerController extends AbstractController
     public function eventIndex(Request $request, PaginatorInterface $paginator, $page_no = null)
     {
         $session = $this->session;
-        $builder = $this->formFactory->createBuilder(CustomerEventType::class);
+        $builder = $this->formFactory->createBuilder(SearchCustomerEventType::class);
         $searchForm = $builder->getForm();
         $pageMaxis = $this->pageMaxRepository->findAll();
         $pageCount = $session->get('eccube.admin.customer_event.search.page_count', $this->eccubeConfig['eccube_default_page_count']);
@@ -75,10 +77,8 @@ class CustomerController extends AbstractController
         }
 
         if ('POST' === $request->getMethod()) {
-
             $searchForm->handleRequest($request);
             if ($searchForm->isValid()) {
-
                 $searchData = $searchForm->getData();
                 $page_no = 1;
                 $session->set('eccube.admin.customer_event.search', FormUtil::getViewData($searchForm));
@@ -130,6 +130,104 @@ class CustomerController extends AbstractController
     }
 
     /**
+     * 顧客イベント登録
+     * @Route("/%eccube_admin_route%/customer/event/new", name="admin_customer_event_new", methods={"GET", "POST"})
+     * @Template("@management/admin/Customer/event/edit.twig")
+     */
+    public function eventRegist(Request $request)
+    {
+        $customerEvent = new CustomerEvent();
+
+        // フォーム作成
+        $builder = $this->formFactory->createBuilder(CustomerEventType::class, $customerEvent);
+        $form = $builder->getForm();
+        $form->handleRequest($request);
+        // 処理ボタン押下
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                // フォーム内容で更新
+                $customerEvent = $form->getData();
+                // 現在日時を設定
+                $customerEvent->setEventStartDate(new \DateTime());
+                $customerEvent->setEventEndDate(new \DateTime());
+                $this->entityManager->persist($customerEvent);
+                $this->entityManager->flush($customerEvent);
+
+                // 成功ログと成功アラート設定
+                log_info('customerEvent edit success');
+                $this->addSuccess('admin.sample.save.complete', 'admin');
+
+                // 更新画面にリダイレクト
+                return $this->redirectToRoute(
+                    'admin_customer_event_edit',
+                    ['id' => $customerEvent->getId()]
+                );
+            } else {
+                // 失敗ログと失敗アラート設定
+                log_info($editform->getErrors(true));
+                $this->addError('admin.customer_event.save.failed', 'admin');
+            }
+        }
+        return [
+            'form' => $form->createView(),
+            'CustomerEvent' => $customerEvent
+        ];
+
+    }
+
+    /**
+     * 顧客イベント更新
+     * @Route("/%eccube_admin_route%/customer/event/{id}/edit", requirements={"id" = "\d+"}, name="admin_customer_event_edit", methods={"GET", "POST"})
+     * @Template("@management/admin/Customer/event/edit.twig")
+     * @param Request リクエスト
+     * @param id 該当データのID
+     */
+    public function eventEdit(Request $request, $id = null)
+    {
+        if (null === $id) {
+            throw new NotFoundHttpException("id is null");
+        }
+
+        $customerEvent = $this->customerEventRepository->find($id);
+        if (null === $customerEvent) {
+            throw new NotFoundHttpException("customerEvent is null");
+        }
+
+        // フォーム作成
+        $builder = $this->formFactory->createBuilder(CustomerEventType::class, $customerEvent);
+        $form = $builder->getForm();
+        $form->handleRequest($request);
+        // 処理ボタン押下
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                // フォーム内容で更新
+                $customerEvent = $form->getData();
+                // 現在日時を設定
+                $this->entityManager->persist($customerEvent);
+                $this->entityManager->flush($customerEvent);
+
+                // 成功ログと成功アラート設定
+                log_info('customerEvent edit success');
+                $this->addSuccess('admin.sample.save.complete', 'admin');
+
+                // 更新画面にリダイレクト
+                return $this->redirectToRoute(
+                    'admin_customer_event_edit',
+                    ['id' => $customerEvent->getId()]
+                );
+            } else {
+                // 失敗ログと失敗アラート設定
+                log_info($editform->getErrors(true));
+                $this->addError('admin.customer_event.save.failed', 'admin');
+            }
+        }
+        return [
+            'form' => $form->createView(),
+            'CustomerEvent' => $customerEvent
+        ];
+    }
+
+    /**
      * 顧客案件一覧
      * @Route("/%eccube_admin_route%/customer/project", name="admin_customer_project", methods={"GET"})
      * @Route("/%eccube_admin_route%/customer/project/page/{page_no}", requirements={"page_no" = "\d+"}, name="admin_customer_project_page", methods={"GET", "POST"})
@@ -141,18 +239,7 @@ class CustomerController extends AbstractController
     }
 
     /**
-     * 顧客イベント登録
-     * @Route("/%eccube_admin_route%/customer/event/new", name="admin_customer_event_new", methods={"GET", "POST"})
-     * @Route("/%eccube_admin_route%/customer/event/{id}/edit", requirements={"id" = "\d+"}, name="admin_customer_event_edit", methods={"GET", "POST"})
-     * @Template("@management/admin/Customer/event/edit.twig")
-     */
-    public function eventEdit(Request $request)
-    {
-        return [];
-    }
-
-    /**
-     * 顧客イベント登録
+     * 顧客案件登録
      * @Route("/%eccube_admin_route%/customer/project/new", name="admin_customer_project_new", methods={"GET", "POST"})
      * @Route("/%eccube_admin_route%/customer/project/{id}/edit", requirements={"id" = "\d+"}, name="admin_customer_project_edit", methods={"GET", "POST"})
      * @Template("@management/admin/Customer/project/edit.twig")
